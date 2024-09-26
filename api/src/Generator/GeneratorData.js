e1d460202c4e090f385adc52a95609e1a6a1dd6a
svetalana <sveta.sharymova@gmail.com>
2024-09-26 18:27:28 +0400

    02:creat generatorData

---
+++ api/src/Generator/GeneratorData.js
@@ -0,0 +1,111 @@
+const { faker } = require('@faker-js/faker');
+
+const {
+	spanishPostalCodes ,
+	spanishFullNames,
+	spanishCities,
+	spanishProvinces,
+	spanishPhoneNumbers,
+	spanishStreets
+} = require('./repo/SpainRepo');
+
+const {ukrainianFullNames,
+	ukrainianRegions,
+	ukrainianCities,
+	ukrainianPhoneNumbers,
+	ukrainianPostalCodes,
+	ukrainianStreets
+} = require('./repo/UkrainRepo');
+
+function generateUserData(region) {
+	switch (region) {
+		case 'ES':
+			faker.locale = 'es';
+			return {
+				// number: faker.number.int({ min: 1, max: 1000 }),
+				identifier: faker.string.uuid(),
+				fullName: faker.helpers.arrayElement(spanishFullNames),
+				address: generateAddress('ES'),
+				phone: generatePhoneNumber('ES'),
+			};
+		case 'UK':
+			faker.locale = 'uk';
+			return {
+				// number: faker.number.int({ min: 1, max: 1000 }),
+				identifier: faker.string.uuid(),
+				fullName: faker.helpers.arrayElement(ukrainianFullNames),
+				address: generateAddress('UK'),
+				phone: generatePhoneNumber('UK'),
+			};
+		case 'US':
+			faker.locale = 'en_US';
+			return {
+				// number: faker.number.int({ min: 1, max: 1000 }),
+				identifier: faker.string.uuid(),
+				fullName: faker.person.fullName(),
+				address: generateAddress('US'),
+				phone: generatePhoneNumber('US'),
+			};
+		default:
+			return null;
+	}
+}
+
+function generateAddress(region) {
+	switch (region) {
+		case 'ES':
+			return faker.helpers.arrayElement([
+				`${faker.helpers.arrayElement(spanishProvinces)}, c. ${faker.helpers.arrayElement(spanishCities)}, st. ${faker.helpers.arrayElement(spanishStreets)}, b. ${faker.number.int({ min: 1, max: 105 })}, ${faker.helpers.arrayElement(spanishPostalCodes)}`,
+				`${faker.helpers.arrayElement(spanishCities)}, st. ${faker.helpers.arrayElement(spanishStreets)}, b. ${faker.number.int({ min: 1, max: 105 })}, flat. ${faker.location.secondaryAddress()}`,
+				`${faker.helpers.arrayElement(spanishCities)}, st. ${faker.location.streetAddress(true)}`,
+				`${faker.helpers.arrayElement(spanishProvinces)}, c.${faker.helpers.arrayElement(spanishCities)}, ${faker.location.streetAddress({ useFullAddress: true })}`,
+			]);
+		case 'UK':
+			return faker.helpers.arrayElement([
+				`р-он ${faker.helpers.arrayElement(ukrainianRegions)}, г. ${faker.helpers.arrayElement(ukrainianCities)}, ул. ${faker.helpers.arrayElement(ukrainianStreets)}, д. ${faker.number.int({ min: 1, max: 105 })}, ${faker.helpers.arrayElement(ukrainianPostalCodes)}`,
+				`${faker.helpers.arrayElement(ukrainianCities)}, ул. ${faker.helpers.arrayElement(ukrainianStreets)}, д. ${faker.number.int({ min: 1, max: 105 }) + "/" + faker.number.int({ min: 1, max: 12 } )}, кв. ${faker.number.int({ min: 1, max: 100 })}`,
+			]);
+		case 'US':
+			return faker.helpers.arrayElement([
+				`${faker.location.state()}, c. ${faker.location.city()}, st. ${faker.location.street()}, b. ${faker.number.int({ min: 1, max: 105 })},zip code:${faker.location.zipCode()}`,
+				`${faker.location.city()}, st. ${faker.location.street()}, b. ${faker.number.int({ min: 1, max: 105 })}, flat. ${faker.location.secondaryAddress()}`,
+				`${faker.location.city()}, st. ${faker.location.streetAddress(true)}`,
+				`${faker.location.city()}, ${faker.location.streetAddress({ useFullAddress: true })}`,
+			]);
+
+
+
+		default:
+			return '';
+	}
+}
+
+function generatePhoneNumber(region) {
+	switch (region) {
+		case 'ES':
+			return faker.helpers.arrayElement([
+				faker.helpers.arrayElement(spanishPhoneNumbers)
+			]);
+		case 'UK':
+			return faker.helpers.arrayElement([
+				faker.helpers.arrayElement(ukrainianPhoneNumbers)
+			]);
+		case 'US':
+			return faker.helpers.arrayElement([
+				faker.phone.number('(###) ###-####'),
+				faker.phone.number('###-###-####')
+			]);
+		default:
+			return '';
+	}
+}
+
+
+// Пример использования для разных стран
+const userES = generateUserData('ES');
+const userUK = generateUserData('UK');
+const userUS = generateUserData('US');
+
+console.log('Spain:', userES);
+console.log('Украiна:', userUK);
+console.log('USA:', userUS);
---
+++ api/src/Generator/repo/SpainRepo.js
@@ -0,0 +1,106 @@
+  const spanishCities = [
+	'Madrid', 'Barcelona', 'Valencia', 'Sevilla', 'Zaragoza',
+	'Málaga', 'Murcia', 'Palma', 'Las Palmas de Gran Canaria', 'Bilbao'
+];
+
+  const spanishProvinces = [
+	'Ávila', 'Badajoz', 'Barcelona', 'Burgos', 'Cáceres',
+	'Cádiz', 'Castellón', 'Ciudad Real', 'Córdoba', 'A Coruña',
+	'Girona', 'Granada', 'Guadalajara', 'Gipuzkoa', 'Huelva',
+	'Huesca', 'Jaén', 'La Rioja', 'Las Palmas', 'León',
+	'Lleida', 'Lugo', 'Madrid', 'Malaga', 'Murcia',
+	'Navarra', 'Ourense', 'Palencia', 'Pontevedra', 'Salamanca',
+	'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Teruel',
+	'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora',
+	'Zaragoza'
+];
+
+
+  const spanishFullNames = [
+	'Alejandro García', 'Beatriz Martínez', 'Carlos Hernández',
+	'Dolores López', 'Eduardo González', 'Francisco Pérez',
+	'Gabriela Sánchez', 'Héctor Ramírez', 'Isabel Torres',
+	'Javier Morales', 'María Ruiz', 'José Alvarez',
+	'Luis Castro', 'Ana Ortega', 'Fernando Vargas'
+];
+  const spanishStreets = [
+	  'Calle Gran Vía',
+	  'Calle de Alcalá',
+	  'Avenida de la Constitución',
+	  'Calle de Preciados',
+	  'Calle de Serrano',
+	  'Avenida de América',
+	  'Calle de Vallehermoso',
+	  'Calle de la Princesa',
+	  'Calle de Génova',
+	  'Avenida de los Reyes Católicos',
+	  'Calle de Bravo Murillo',
+	  'Calle de Vallehermoso',
+	  'Calle de Fuencarral',
+	  'Avenida del General Perón',
+	  'Calle de Ríos Rosas',
+	  'Calle de Hortaleza',
+	  'Avenida de la Paz',
+	  'Calle del Turia',
+	  'Calle de Chueca',
+	  'Calle de la Montera',
+	  'Calle de Vallehermoso',
+	  'Calle del Arenal',
+	  'Avenida de la Libertad',
+	  'Calle del Carmen',
+	  'Calle de la Princesa',
+	  'Calle de Goya',
+	  'Avenida de la Ilustración',
+	  'Calle del Doctor Esquerdo',
+	  'Calle de San Bernardo',
+	  'Calle de Santa Engracia',
+	  'Avenida de los Doctores Castroviejo',
+	  'Calle de Pedro Zerolo',
+	  'Calle de Colón',
+	  'Calle de Atocha',
+	  'Avenida de la Albufera',
+	  'Calle del Marqués de Vadillo',
+	  'Calle del Conde de Peñalver',
+	  'Calle de Ortega y Gasset',
+	  'Calle del Vallehermoso',
+	  'Calle de los Madrazo',
+	  'Calle de la Sagrera',
+	  'Calle de Juan Bravo',
+	  'Calle de Enrique Granados',
+	  'Calle de Miguel Ángel',
+	  'Calle de Ávila',
+	  'Calle de Sor Ángela de la Cruz'
+  ];
+
+
+  const spanishPhoneNumbers = [
+	'91 123 4567',
+	'93 234 5678',
+	'96 345 6789',
+	'95 456 7890',
+	'91 987 6543',
+	'612 345 678',
+	'611 234 567',
+	'637 890 123',
+	'691 234 567',
+	'676 543 210',
+];
+
+  const spanishPostalCodes = [
+	'28001', '08001', '29001', '41001', '06001',
+	'46001', '33001', '50001', '35001', '38001',
+	'08002', '28002', '29002', '41002', '06002',
+	'46002', '33002', '50002', '35002', '38002',
+	'08003', '28003', '29003', '41003', '06003',
+	'46003', '33003', '50003', '35003', '38003'
+];
+
+module.exports = {
+	spanishCities,
+	spanishProvinces,
+	spanishFullNames,
+	spanishPhoneNumbers,
+	spanishPostalCodes,
+	spanishStreets
+};
+
