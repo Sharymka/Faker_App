78dab2bc05a33178db80f108fcc1107437a03058
svetalana <sveta.sharymova@gmail.com>
2024-09-30 14:19:36 +0400

    10 :add commands into package.json and add env.PORT

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {generateUserData} = require('./src/GeneratorData/mainGenerator');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const api = express.Router();
 app.use('/api', api);
 api.use(express.json());
